c557856845b5be66c2ce1e802c103a863d1625d0
GacinhoV33 <fgacek@student.agh.edu.pl>
2021-05-18 22:11:55 +0200

    last_init

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import watchdog.observers
-from watchdog.events import FileSystemEventHandler
-
 import os
 import time
 import datetime
@@ -8,27 +5,7 @@ import pathlib
 import xlrd
 from shutil import copy
 
-#
-# class MyHandler(FileSystemEventHandler):
-#     def on_modified(self, event):
-#         for filename in os.listdir(folder_name):
-#             src = folder_name + "/" + filename
-#             if ".txt" in filename:
-#                 new_destination = folder_destination_txt + "/" + filename
-#                 if filename in os.listdir(folder_destination_txt):
-#                     new_destination += "_"
-#                 os.rename(src, new_destination)
-#
-#             elif ".png" or ".jpg" or ".bmp" in filename:
-#                 new_destination = folder_destination_png + "/" + filename
-#                 if filename in os.listdir(folder_destination_png):
-#                     new_destination[-5:] += "_.jpg"
-#                 os.rename(src, new_destination)
-#             elif ".pdf" or ".doc" or ".csv" in filename:
-#                 new_destination = folder_destination_pdfdoccsv + "/" + filename
-#                 os.rename(src, new_destination)
-#             # else:
-#             #     new_destination = folder_destination_rest + "/" + filename
+
 ext_list = [".txt", ".png", ".jpg", ".doc", ".pdf", ".bmp", ".csv", ".jpeg"]
 images_ext = [".png", ".jpg", ".jpeg", ".bmp"]
 doc_ext = [".doc", ".pdf", ".csv"]
@@ -47,7 +24,6 @@ def check_path_folders(path):
 
 def sort_files(initial_path: str, flag=True):
     check_path_folders(initial_path)
-    # print([f for f in os.listdir(initial_path)])
     for filename in [f for f in os.listdir(initial_path) if "." in f]:
         if ".txt" in filename:
             modification_time = datetime.datetime.fromtimestamp(
@@ -61,7 +37,6 @@ def sort_files(initial_path: str, flag=True):
                     flag = False
             if not os.path.exists(initial_path + "/txt_files/" + str(modification_time)):
                 os.makedirs(initial_path + "/txt_files/" + str(modification_time))
-            # os.rename(initial_path + "/" + filename, initial_path + "/txt_files/" + str(modification_time) + "/" +new_name)
             os.rename(os.path.join(initial_path, filename),
                       os.path.join(initial_path, "txt_files", str(modification_time), new_name))
 
@@ -85,46 +60,50 @@ def search_file_by_ext(ext: str, init_path: str, dest_path: str, days, start_tim
                 if ext in images_ext:
                     if not os.path.exists(dest_path + "/" + lect_name + "/images/" + str(modification_time.date())):
                         os.makedirs(dest_path + "/" + lect_name + "/images/" + str(modification_time.date()))
-                    # while flag:
-                    #     if new_filename in os.listdir(dest_path + "/" + lect_name + "/images/" + str(modification_time.date())):
-                    #         new_filename = "(1)" + new_filename
-                    #     else:
-                    #         flag = False TODO -> probably not necessary
-                    copy(init_path + "/" + filename, dest_path + "/" + lect_name + "/images/" + str(modification_time.date()))
+
+                    copy(init_path + "/" + filename,
+                         dest_path + "/" + lect_name + "/images/" + str(modification_time.date()))
                 elif ext in doc_ext:
                     if not os.path.exists(dest_path + "/" + lect_name + "/documents/" + str(modification_time.date())):
                         os.makedirs(dest_path + "/" + lect_name + "/documents/" + str(modification_time.date()))
-                    copy(init_path + "/" + filename, dest_path + "/" + lect_name + "/documents/" + str(modification_time.date()))
+                    copy(init_path + "/" + filename,
+                         dest_path + "/" + lect_name + "/documents/" + str(modification_time.date()))
                 else:
-                    if not os.path.exists(dest_path + "/" + lect_name + "/" + ext[1:] + "_files/" + str(modification_time.date())):
-                        os.makedirs(dest_path + "/" + lect_name + "/" + ext[1:] + "_files/" + str(modification_time.date()))
+                    if not os.path.exists(
+                            dest_path + "/" + lect_name + "/" + ext[1:] + "_files/" + str(modification_time.date())):
+                        os.makedirs(
+                            dest_path + "/" + lect_name + "/" + ext[1:] + "_files/" + str(modification_time.date()))
                     while flag:
-                        if new_filename in os.listdir(dest_path + "/" + lect_name + "/" + ext[1:] + "_files/" + str(modification_time.date())):
+                        if new_filename in os.listdir(dest_path + "/" + lect_name + "/" + ext[1:] + "_files/" + str(
+                                modification_time.date())):
                             new_filename = "(1)" + new_filename
                         else:
                             flag = False
-                    copy(init_path + "/" + filename, dest_path + "/" + lect_name + "/" + ext[1:] + "_files/" + str(modification_time.date()))
+                    copy(init_path + "/" + filename,
+                         dest_path + "/" + lect_name + "/" + ext[1:] + "_files/" + str(modification_time.date()))
 
 
 def segregating_lectures():
     lectures_data = xlrd.open_workbook("Data.xls")
-    arkusz_lectures = lectures_data.sheet_by_name(lectures_data.sheet_names()[0])   #choosing correct sheet from csv file
+    arkusz_lectures = lectures_data.sheet_by_name(
+        lectures_data.sheet_names()[0])  # choosing correct sheet from csv file
     lectures_names = list()
     start_time = list()
     end_time = list()
     days = list()
     n_rows = arkusz_lectures.nrows
-    init_path1 = arkusz_lectures.row_values(1)[5] #init_path is path of folder where files are downloaded
+    init_path1 = arkusz_lectures.row_values(1)[5]  # init_path is path of folder where files are downloaded
     # usually it's downloads or folder in documents where screenshots appeared
     init_path2 = arkusz_lectures.row_values(2)[5]
-    dest_path = arkusz_lectures.row_values(1)[6] #dest_path is destination where you want store data
+    dest_path = arkusz_lectures.row_values(1)[6]  # dest_path is destination where you want store data
 
     for i in range(1, n_rows):
         # Lectures
         lectures_names.append(arkusz_lectures.row_values(i)[0])
 
         # Time start
-        data_values = xlrd.xldate_as_datetime(float(arkusz_lectures.row_values(i)[1]), lectures_data.datemode)  #the time conversation
+        data_values = xlrd.xldate_as_datetime(float(arkusz_lectures.row_values(i)[1]),
+                                              lectures_data.datemode)  # the time conversation
         # is necessary, otherwise you will receive wrong format of time
         start_time.append(data_values.time())
 
@@ -135,61 +114,18 @@ def segregating_lectures():
         # Days
         days.append(arkusz_lectures.row_values(i)[3])
 
-     #list of extensions that you wanna copy/sort
+    # list of extensions that you wanna copy/sort
 
     for ext in ext_list:
         search_file_by_ext(ext, init_path1, dest_path, days, start_time, end_time, lectures_names, n_rows)
         search_file_by_ext(ext, init_path2, dest_path, days, start_time, end_time, lectures_names, n_rows)
 
 
-try:
-    while True:
-        segregating_lectures()
-        time.sleep(60)
-except:
-    raise RuntimeError("Some undefinied actions occured")
+segregating_lectures()
 
-#
-
-# def create_name_folders():
-#     pass
-#
-#
-# def swap_folders(initial_path:str, destination_path:str):
-#
-#     if not
-#         create_name_folders()
-
-# sort_files("C:/Users/gacek/Downloads/txt_files")
-
-
-# for downloads
-# folder_name2 = "C:/Users/gacek/Downloads"
-# folder_destination_txt = "
-# folder_destination_png = "C:/Users/gacek/Downloads/images_files"
-# folder_destination_rest = "C:/Users/gacek/Downloads/rest"
-# folder_destination_pdfdoccsv = "C:/Users/gacek/Downloads/pdf"
-# event_handler = MyHandler()
-# observer = watchdog.observers.Observer()
-# observer.schedule(event_handler, folder_name2, recursive=True)
-# observer.start()
-
-
-# # some folder from computer
-# folder_name = "C:/Users/gacek/Desktop/Projekty IT/Python/initial"
-# folder_destination_txt = "C:/Users/gacek/Desktop/Projekty IT/Python/txt_files"
-# folder_destination_png = "C:/Users/gacek/Desktop/Projekty IT/Python/images_files"
-# folder_destination_rest = "C:/Users/gacek/Desktop/Projekty IT/Python/rest"
-# folder_destination_pdfdoccsv = "C:/Users/gacek/Desktop/Projekty IT/Python/pdf"
-# event_handler = MyHandler()
-# observer = watchdog.observers.Observer()
-# observer.schedule(event_handler, folder_name, recursive=True)
-# observer.start()
-#
 # try:
 #     while True:
-#         time.sleep(10)
-# except KeyboardInterrupt:
-#     observer.stop()
-#
-# observer.join()
+#         segregating_lectures()
+# except:
+#     raise RuntimeError("Some undefinied actions occured")
+
